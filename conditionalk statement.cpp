088102a07238c8a43c5ce984ba5b5b1da71318f7
Saurabh Pandey <119298223+Saurabh8227@users.noreply.github.com>
2024-06-19 10:23:15 +0530

    Add files via upload

---
+++ conditionalk statement.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b;
+	int c;
+	int d;
+	cout<<"Enter the four interger a , b , c , d"<<endl;
+	cin>> a;
+	cin>> b;
+	cin>> c;
+	cin>>d;
+	cout<<(a<b && c>d); 
+	
+}
+
